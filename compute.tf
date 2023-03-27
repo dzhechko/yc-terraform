69ca5757e5a6fc26b191a7f44243b3c4b14afb48
Dmitriy Zhechkov <jechkov.dmitriy@gmail.com>
2023-03-27 13:04:36 +0300

    Add files via upload

---
+++ compute.tf
@@ -0,0 +1,59 @@
+# =================
+# Compute Resources
+# =================
+
+# https://registry.terraform.io/providers/yandex-cloud/yandex/latest/docs/data-sources/datasource_compute_image
+
+data "yandex_compute_image" "vm_image" {
+  family = "ubuntu-2004-lts"
+}
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "terraform1"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      #image_id = "fd83klic6c8gfgi40urb"
+      image_id = data.yandex_compute_image.vm_image.id
+      #image_id = "${data.yandex_compute_image.vm_image.id}"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+resource "yandex_compute_instance" "vm-2" {
+  name = "terraform2"
+
+  resources {
+    cores  = 4
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "${data.yandex_compute_image.vm_image.id}"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
